ab6f54730956b283b59fa3d436a518822cbc166e
Chidozie Ogbo <Chidozie Ogbo>
2017-09-02 16:24:14 +0100

    init

---
+++ NETGroupSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace NETGroupSignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private static int count = 0;
+        public override Task OnConnected()
+        {
+            count++;
+            Clients.All.updateCount(count);
+            return base.OnConnected();
+        }
+		
+		public void Join(string name)
+        {
+            Clients.Others.userJoined(name);
+        }
+
+        public void SendMessage(string name, string message)
+        {
+            Clients.Others.onReceiveMessage(name, message);
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            count--;
+            Clients.All.updateCount(count);
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
---
+++ NETGroupSignalR/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NETGroupSignalR.Startup))]
+namespace NETGroupSignalR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            app.MapSignalR();
+        }
+    }
+}
